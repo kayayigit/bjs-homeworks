7842364ceb506f1f14235071b69ef332100181e3
Yigit Kaya <kayayigit2218@gmail.com>
2021-02-28 22:39:36 +0300

    first commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,44 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict";
+    let D = (b**2) - (4 * a * c);
+    let firstRoot = (-b + Math.sqrt(D)) / (2 * a);
+    let secondRoot = (-b - Math.sqrt(D)) / (2 * a);
+    if (D > 0) {
+        return [firstRoot, secondRoot]
+    } else if (D === 0) {
+        return [firstRoot];
+    } else {
+        return[];
+    }
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+ 
+    marks.splice (5);
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    let averageMark = sum / marks.length;
+
+    if (marks.length === 0) {
+        return 0;
+    } else {
+        return averageMark;
+    }
+
 }
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
-    // return result;
+    let birthYear = dateOfBirthday.getFullYear();
+    const messageForOld = `Не желаете ли олд-фэшн, ${name}?`;
+    const messageForYoung = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
+    let today = new Date ();
+    let currentYear = today.getFullYear();
+    if ((currentYear - birthYear) > 18) {
+        return messageForOld;
+    } else {
+        return messageForYoung;
+    }
 }
